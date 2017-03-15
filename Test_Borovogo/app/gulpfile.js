8a996a2d3466cb4bf512dd115370167cf9481ae6
happy-Mary <mariashklovska1991@gmail.com>
2017-03-15 12:57:49 +0300

    Test - Header done

---
+++ Test_Borovogo/app/gulpfile.js
@@ -0,0 +1,56 @@
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    concatCss = require('gulp-concat-css'),
+    cleanCSS = require('gulp-clean-css'),
+    rename = require("gulp-rename"),
+    notify = require("gulp-notify"),
+    autoprefixer = require('gulp-autoprefixer'),
+    livereload = require('gulp-livereload'),
+    connect = require('gulp-connect'),
+    less = require('gulp-less')
+
+
+// html
+gulp.task('html', function() {
+    gulp.src('index.html')
+        .pipe(connect.reload());
+});
+
+// server
+gulp.task('connect', function() {
+    connect.server({
+        root: '',
+        livereload: true
+    });
+});
+
+
+// css
+gulp.task('css', function() {
+    return gulp.src('css/**/*.css')
+        .pipe(concatCss("bundle.css"))
+        .pipe(autoprefixer({
+            browsers: ['last 3 versions', '> 5%']
+        }))
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(rename("bundle.min.css"))
+        .pipe(gulp.dest("css-ready"))
+        .pipe(connect.reload())
+        .pipe(notify("DONE!"));
+});
+
+// less
+gulp.task("less", function() {
+    return gulp.src('less/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('css'))
+        .pipe(livereload());
+});
+
+gulp.task('watch', function() {
+    gulp.watch('less/*.less', ['less'])
+    gulp.watch('css/*.css', ['css'])
+    gulp.watch('index.html', ['html'])
+});
+
+gulp.task('default', ['connect', 'watch']);
