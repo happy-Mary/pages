39b3016ec19631f39c9b1ec9ba141616ba93b0fb
happy-Mary <mariashklovska1991@gmail.com>
2017-05-26 00:13:47 +0300

    test template finished

--- Test_Myfin/app/script.js
+++ Test_Myfin/app/script.js
@@ -15,7 +15,7 @@ function changeBorder(){
 	}
 }
 
-// pattern for input text with numbers
+// pattern for input[text] with numbers
 var numFieldPattern = /^[0-9]+$/;
 var numPattern = /[0-9]/;
 
@@ -34,3 +34,57 @@ for(var i=0; i<costFieds.length; i++) {
 	  return numPattern.test(e.key);
 	}
 }
+
+// open and close modal window
+var closeIcon = document.querySelector(".closeModal");
+var modalWindow =  document.querySelector(".modalArea");
+var openButton = document.querySelector(".openModal");
+
+closeIcon.addEventListener('click', closeModalWindow, false);
+openButton.addEventListener('click', openModalWindow, false);
+
+function closeModalWindow(e){
+	e.preventDefault;
+	modalWindow.style.display="none";
+}
+
+function openModalWindow(e){
+	e.preventDefault;
+	modalWindow.style.display="block";
+}
+
+
+// jQuery-UI Slider
+$( function() {
+
+	function setSlider(sliderId, input1, input2, min, max, value2) {
+		 var sliderSettings = {
+	      range: true,
+	      animate: "slow",
+	      min: min,
+	      max: max,
+	      values: [ min, value2 ],
+	      slide: function( event, ui ) {
+	        $(input1).val( ui.values[ 0 ]);
+	        $(input2).val( ui.values[ 1 ]);
+	      }
+    	}
+
+		$( sliderId ).slider(sliderSettings);
+
+	    $( input1 ).val( $( sliderId ).slider( "values", 0 ));
+	    $( input2 ).val( $( sliderId ).slider( "values", 1 ));
+
+	    $( input1 ).keyup(function() {
+			$( sliderId ).slider( "values", 0, $(this).val() );
+		});
+
+		$( input2 ).keyup(function() {
+			$( sliderId ).slider( "values", 1, $(this).val() );
+		});
+	}
+
+	setSlider("#slider-range-1", "#dist-num1", "#dist-num2", 0, 20, 8);
+	setSlider("#slider-range-2", "#area-num1", "#area-num2", 30, 200, 80);
+			
+});
