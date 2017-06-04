df425e59464c711a33908ca2ec3e6bfa730de65c
happy-Mary <mariashklovska1991@gmail.com>
2017-06-05 00:32:38 +0300

    http requests and filters

--- Test_System Technologies/app/js/app.js
+++ Test_System Technologies/app/js/app.js
@@ -13,16 +13,26 @@ function mainController($scope, $location) {
     }
 };
 
-function appController($scope, newsResources, objectFactory, $http, productService) {
-    // $scope.newsRes = newsResources.data.sources; 
+function appController($scope, newsResources, $state) {
     $scope.newsRes = newsResources.sources; 
-    // productService.addProduct($scope.newsRes);
+    console.log($scope.newsRes);
+
+    $scope.resourceContent = 'all';
+    $scope.categotyContent = 'all';
+    $scope.limitToShow = 1;
+
+    $scope.showResource = function(name, category){
+        $scope.resourceContent = name;
+        $scope.categotyContent = category;
+        $scope.limitToShow = 10;
+    }
+
+
 
     $scope.categories = [];
     $scope.makeCatArr = function(){
         for (var i = $scope.newsRes.length - 1; i >= 0; i--) {
             var catItem = String($scope.newsRes[i].category);
-            // console.log(catItem);
             function Some(V, I, A){
                 return V.localeCompare(catItem) === 0;
             }
@@ -30,14 +40,27 @@ function appController($scope, newsResources, objectFactory, $http, productServi
             var res = $scope.categories.some(Some);
             if(res === false) {$scope.categories.push(catItem);}
             }
-            // console.log($scope.categories);
       };
     $scope.makeCatArr();
+
+    $scope.currentArticle = {title: "Choose an article"};
+    $scope.readMore = function(string){
+        $scope.currentArticle = string;
+       
+        $state.go("app.news-info");
+         console.log($scope.currentArticle);
+    }
+
+    $scope.$watch('currentArticle', function(newVal, oldVal){
+        console.log('changed');
+    }, true);
+
+
 }
 
-function newsController($scope, newsArticles){
-    console.log(newsArticles);
-    // исправить получение объекта
+function newsController($scope, $state, objectFactory, newsResources){
+    console.log(newsResources);
+    $scope.allArticles = newsResources;
 }
 
 
@@ -64,9 +87,9 @@ newsApp.config(function($stateProvider, $urlRouterProvider, $urlMatcherFactoryPr
             templateUrl: "templates/info.html",
             controller: "NewsCtrl",
             resolve: {
-            newsArticles: function(productService, objectFactory, newsResources){
-               return productService.addResourceforArt(newsResources.sources);
-                }
+            newsResources: function(dataService){
+               return dataService.getData();
+            },
             }
         })
         .state("app.saved", {
@@ -82,34 +105,14 @@ newsApp.config(function($stateProvider, $urlRouterProvider, $urlMatcherFactoryPr
         	templateUrl: "templates/added-news.html"
         })
         .state("app.news-info", {
-        	url: "/article/{id:[/d]{1,20}}",
+        	// url: "/article/{id:[/d]{1,20}}",
+            url: "/article",
         	templateUrl: "templates/news-info.html"
         });
 });
 
-
-
-// SERVISE for getting all articles
-newsApp.service('productService', function(objectFactory) {
-  var sourceList = null;
-
-  var addResourceforArt = function(newObj) {
-      sourceList = newObj;
-      console.log("sourceList updated");
-      console.log(sourceList.length);
-      return objectFactory.getArticle(sourceList);
-  };
-
-  return {
-    addResourceforArt: addResourceforArt
-  };
-
-});
-
-
 // GETTING SOURCES
-newsApp.factory("objectFactory", ['$http', function($http){  
-
+newsApp.factory("objectFactory", function($http, dataService){  
     var obj = {
         getObject: function(dataUrl){
             var dataUrl = String(dataUrl);
@@ -117,30 +120,64 @@ newsApp.factory("objectFactory", ['$http', function($http){
                 method: 'GET',
                 url: dataUrl
                 }).then(function(data, status, headers, config) {
+                    dataService.setData(data.data);
                     return data.data;
+                
             });
             return promise1;
-        },
-        getArticle: function(dataRes){
-            var dataRes =  dataRes;
-            var articlesArr = [];
-
-            for(var i = 0; i<=dataRes.length-1; i++){
-                var sourceId = dataRes[i].id;
-                
-                var sort = dataRes[i].sortBysAvailable[0];
-                var api_KEY = '3ce5eede372b41dca1278128350bf064';
-                var dataUrl = 'https://newsapi.org/v1/articles?source='+sourceId+'&sortBy='+sort+'&apiKey='+api_KEY;   
-
-                $http.get(dataUrl).then(function(response){articlesArr.push(response.data); return articlesArr;});
-            } //FOR finish
-             return articlesArr;
         }
-
     };
    
     return obj;
-}]);
+});
 
 
+// Getting Articles
+newsApp.factory('myService', function ($http, $q){
+  return {
+    getItems: function (resArr){
+        var resArr = resArr.sources;
+        // console.log(resArr);
+        var promises= [];
+        for (var i = 0; i <= resArr.length-1; i++) {
 
+            var sourceId = resArr[i].id;
+            var sort = resArr[i].sortBysAvailable[0];
+            var api_KEY = '3ce5eede372b41dca1278128350bf064';
+            var dataUrl = 'https://newsapi.org/v1/articles?source='+sourceId+'&sortBy='+sort+'&apiKey='+api_KEY;   
+
+            var promise=$http.get(dataUrl);
+            promises.push(promise);
+        }
+
+       return $q.all(promises)
+      .then(function(results) {
+        var data = [];
+        angular.forEach(results, function(result) {
+          data = data.concat(result.data);
+        });
+        return data;
+      });
+    }
+  };
+});
+
+
+// SERVISE for getting all articles
+newsApp.factory('dataService', function (myService) {
+    var formData = {};
+
+    return {
+        getData: function () {
+             return myService.getItems(formData);
+        },
+        setData: function (newFormData) {
+            formData = newFormData
+        },
+        resetData: function () {
+            formData = {};
+        }
+    };
+});
+
+    
